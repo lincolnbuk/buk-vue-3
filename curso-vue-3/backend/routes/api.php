2d0a7f4df321ec4bce48c741e467287d15ade760
lincolnbuk <sit-bull@hotmail.com>
2023-06-21 20:37:14 -0300

    Configuracoes do backend

--- curso-vue-3/backend/routes/api.php
+++ curso-vue-3/backend/routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// api/users (o frontend, vai ter o retorno abaixo)
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/users', function () {
+    return User::all();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
